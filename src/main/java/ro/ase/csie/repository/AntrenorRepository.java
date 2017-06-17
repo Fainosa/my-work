1ef15d609dddd53f4e040f7455bc9b7d4ab4c18b
andra.popescu <andra.popescu@metrosystems.net>
2017-06-17 22:30:52 +0300

    repository for antrenor

---
+++ src/main/java/ro/ase/csie/repository/AntrenorRepository.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ro.ase.csie.licenta.domain.Antrenor;
+
+public interface AntrenorRepository extends JpaRepository<Antrenor, Long> {
+
+}

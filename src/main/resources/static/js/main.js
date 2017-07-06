d046a391f386acdb13b9cbcf6827f661ea08ce25
andra.popescu <andra.popescu@metrosystems.net>
2017-07-06 11:01:40 +0300

    graph

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,3 +1,8 @@
+$(document).ready(function() {
+
+});
+
+
 $(function() {
   if ($.browser.msie && $.browser.version.substr(0,1)<7)
   {
@@ -66,79 +71,5 @@ var x = setInterval(function() {
 
 
 
-var getDataUrl = "http://localhost:8088/graph/clasament-echipe";
 
-$(document).ready(function() {
-    // getData(getDataUrl);
-    $("body").append('<div id="graph"></div>');
-    // $("body").append('<div id="clasamentEchipa"></div>');
-    
-    drawLine()
 
-});
-
-function drawLine1(data) {
-    new Morris.Bar({
-        barGap : 4,
-        barSizeRatio : 0.7,
-        element : 'clasamentEchipa',
-        data : data,
-        xkey: 'x',
-        ykeys: ['y'],
-        labels: ['Y'],
-        barColors: function (row, series, type) {
-            if (type === 'bar') {
-                var red = Math.ceil(255 * row.y / this.ymax);
-                return 'rgb(' + red + ',0,0)';
-            }
-            else {
-                return '#000';
-            }
-        }
-    });
-}
-function drawLine() {
-    Morris.Bar({
-        element: 'graph',
-        data: [
-            {x: 'Boston Celtics', y: 25},
-            {x: 'Brooklyn Nets', y: 29},
-            {x: 'Chicago Bulls', y: 48},
-            {x: 'Clevenad Cavaliers', y: 84},
-            {x: 'Miami Heat', y: 33},
-            {x: 'Orlango Magic', y: 45},
-            {x: 'Denver Nuggets', y: 20},
-            {x: 'Washington Wizards', y: 50},
-            {x: 'New York Knicks', y: 73}
-        ],
-        xkey: 'x',
-        ykeys: ['y'],
-        labels: ['Puncte'],
-        barColors: function (row, series, type) {
-            if (type === 'bar') {
-                var red = Math.ceil(255 * row.y / this.ymax);
-                return 'rgb(' + red + ',0,0)';
-            }
-            else {
-                return '#000';
-            }
-        }
-    });
-}
-function getData(api) {
-    $.ajax({
-        url : api,
-        type : 'GET',
-        dataType : 'json',
-        async : false,
-        success : function(data) {
-            alert("fgnmfgh")
-            console.log(data);
-
-                drawLine1(data)
-        },
-        error : function(xhr, message, errorThrown) {
-            alert(errorThrown);
-        }
-    });
-}

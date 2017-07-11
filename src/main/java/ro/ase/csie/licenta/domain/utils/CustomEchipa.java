c9bdf08673496c0f1b1bae4235228ed0a9f0b5d5
andra.popescu <andra.popescu@metrosystems.net>
2017-07-12 00:13:26 +0300

    data object with custom object

---
+++ src/main/java/ro/ase/csie/licenta/domain/utils/CustomEchipa.java
@@ -0,0 +1,72 @@
+package ro.ase.csie.licenta.domain.utils;
+
+public class CustomEchipa {
+
+   String nume;
+   int cosuriMarcate;
+   int cosuriPrimite;
+   int victorii;
+   int infrangeri;
+   int punctaj;
+
+    public CustomEchipa( String nume, int cosuriMarcate, int cosuriPrimite, int victorii, int infrangeri, int punctaj ) {
+        this.nume = nume;
+        this.cosuriMarcate = cosuriMarcate;
+        this.cosuriPrimite = cosuriPrimite;
+        this.victorii = victorii;
+        this.infrangeri = infrangeri;
+        this.punctaj = punctaj;
+    }
+
+    public CustomEchipa() {
+
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume( String nume ) {
+        this.nume = nume;
+    }
+
+    public int getCosuriMarcate() {
+        return cosuriMarcate;
+    }
+
+    public void setCosuriMarcate( int cosuriMarcate ) {
+        this.cosuriMarcate = cosuriMarcate;
+    }
+
+    public int getCosuriPrimite() {
+        return cosuriPrimite;
+    }
+
+    public void setCosuriPrimite( int cosuriPrimite ) {
+        this.cosuriPrimite = cosuriPrimite;
+    }
+
+    public int getVictorii() {
+        return victorii;
+    }
+
+    public void setVictorii( int victorii ) {
+        this.victorii = victorii;
+    }
+
+    public int getInfrangeri() {
+        return infrangeri;
+    }
+
+    public void setInfrangeri( int infrangeri ) {
+        this.infrangeri = infrangeri;
+    }
+
+    public int getPunctaj() {
+        return punctaj;
+    }
+
+    public void setPunctaj( int punctaj ) {
+        this.punctaj = punctaj;
+    }
+}

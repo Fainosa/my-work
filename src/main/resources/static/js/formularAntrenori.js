d502621fa23dfd225041ce83d16a2b4dc5161ee7
Andra Popescu <andraa.popescu.94@gmail.com>
2017-07-13 21:43:30 +0300

    drop-down echipe

--- src/main/resources/static/js/formularAntrenori.js
+++ src/main/resources/static/js/formularAntrenori.js
@@ -1,166 +1,51 @@
+var echipeUrl = 'http://localhost:8088/echipa/all';
+
 $(document).ready(function() {
- var table = $('#example').DataTable();
+   getData(echipeUrl);
 
-  // Get the modal
-  var modal = document.getElementById('myModal');
 
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-      modal.style.display = "none";
-  }
-
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-  }
-
-$(function() {
-  if ($.browser.msie && $.browser.version.substr(0,1)<7)
-  {
-    $('li').has('ul').mouseover(function(){
-        $(this).children('ul').css('visibility','visible');
-        }).mouseout(function(){
-        $(this).children('ul').css('visibility','hidden');
-        })
-  }
 });
 
-/* Mobile */
-$('#menu-wrap').prepend('<div id="menu-trigger">Menu</div>');
-$("#menu-trigger").on("click", function(){
-    $("#menu").slideToggle();
+function getData(api) {
+    $.ajax({
+        url : api,
+        type : 'GET',
+        dataType : 'json',
+        async : false,
+        success : function(data) {
+           populateDropdown(data);
+        },
+        error : function(xhr, message, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
+
+function populateDropdown(data) {
+    var myOption = [];
+
+     $.each(data, function(i, item) {
+           myOption[i] = item.numeEchipa;
+        });
+
+    var items;
+    for (var i=0; i< myOption.length; i++){
+        items += "<option>" + myOption[i] + "</option>";
+    }
+
+    $("#drop-down").append(items);
+}
+
+//$("#btn_submit" ).click(function() {
+//         alert("fghjkfgh");
+//
+//          var  nume = $('#nume').val();
+//          alert(nume);
+//
+//     });
+
+$("#btn_submit" ).on( "click", function() {
+    alert("fghjkfgh");
 });
 
-// iPad
-var isiPad = navigator.userAgent.match(/iPad/i) != null;
-if (isiPad) $('#menu ul').addClass('no-transition');
-
-$(window).scroll(function(e){
-  var $el = $('.fixedElement');
-  var isPositionFixed = ($el.css('position') == 'fixed');
-  if ($(this).scrollTop() > 200 && !isPositionFixed){
-    $('.fixedElement').css({'position': 'fixed', 'top': '0px'});
-  }
-  if ($(this).scrollTop() < 200 && isPositionFixed)
-  {
-    $('.fixedElement').css({'position': 'static', 'top': '0px'});
-  }
-});
-
-var nativePicker = document.querySelector('.nativeDatePicker');
-var fallbackPicker = document.querySelector('.fallbackDatePicker');
-var fallbackLabel = document.querySelector('.fallbackLabel');
-
-var yearSelect = document.querySelector('#year');
-var monthSelect = document.querySelector('#month');
-var daySelect = document.querySelector('#day');
-
-// hide fallback initially
-fallbackPicker.style.display = 'none';
-fallbackLabel.style.display = 'none';
-
-// test whether a new date input falls back to a text input or not
-var test = document.createElement('input');
-test.type = 'date';
-// if it does, run the code inside the if() {} block
-if(test.type === 'text') {
-  // hide the native picker and show the fallback
-  nativePicker.style.display = 'none';
-  fallbackPicker.style.display = 'block';
-  fallbackLabel.style.display = 'block';
-
-  // populate the days and years dynamically
-  // (the months are always the same, therefore hardcoded)
-  populateDays(monthSelect.value);
-  populateYears();
-}
-
-function populateDays(month) {
-  // delete the current set of <option> elements out of the
-  // day <select>, ready for the next set to be injected
-  while(daySelect.firstChild){
-    daySelect.removeChild(daySelect.firstChild);
-  }
-
-  // Create variable to hold new number of days to inject
-  var dayNum;
-
-  // 31 or 30 days?
-  if(month === 'January' | month === 'March' | month === 'May' | month === 'July' | month === 'August' | month === 'October' | month === 'December') {
-    dayNum = 31;
-  } else if(month === 'April' | month === 'June' | month === 'September' | month === 'November') {
-    dayNum = 30;
-  } else {
-  // If month is February, calculate whether it is a leap year or not
-    var year = yearSelect.value;
-    (year - 2016) % 4 === 0 ? dayNum = 29 : dayNum = 28;
-  }
-
-  // inject the right number of new <option> elements into the day <select>
-  for(i = 1; i <= dayNum; i++) {
-    var option = document.createElement('option');
-    option.textContent = i;
-    daySelect.appendChild(option);
-  }
-
-  // if previous day has already been set, set daySelect's value
-  // to that day, to avoid the day jumping back to 1 when you
-  // change the year
-  if(previousDay) {
-    daySelect.value = previousDay;
-
-    // If the previous day was set to a high number, say 31, and then
-    // you chose a month with less total days in it (e.g. February),
-    // this part of the code ensures that the highest day available
-    // is selected, rather than showing a blank daySelect
-    if(daySelect.value === "") {
-      daySelect.value = previousDay - 1;
-    }
-
-    if(daySelect.value === "") {
-      daySelect.value = previousDay - 2;
-    }
-
-    if(daySelect.value === "") {
-      daySelect.value = previousDay - 3;
-    }
-  }
-}
-
-function populateYears() {
-  // get this year as a number
-  var date = new Date();
-  var year = date.getFullYear();
-
-  // Make this year, and the 100 years before it available in the year <select>
-  for(var i = 0; i <= 100; i++) {
-    var option = document.createElement('option');
-    option.textContent = year-i;
-    yearSelect.appendChild(option);
-  }
-}
-
-// when the month or year <select> values are changed, rerun populateDays()
-// in case the change affected the number of available days
-yearSelect.onchange = function() {
-  populateDays(monthSelect.value);
-}
-
-monthSelect.onchange = function() {
-  populateDays(monthSelect.value);
-}
-
-//preserve day selection
-var previousDay;
-
-// update what day has been set to previously
-// see end of populateDays() for usage
-daySelect.onchange = function() {
-  previousDay = daySelect.value;
-}
 
